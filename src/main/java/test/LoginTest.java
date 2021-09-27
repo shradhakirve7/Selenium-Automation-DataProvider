3938fc2114dc44e52961cbecab87de360bd98168
shradha <shkirve@gmail.com>
2021-09-27 16:27:22 +0530

    User login with data Provider

---
+++ src/main/java/test/LoginTest.java
@@ -0,0 +1,22 @@
+package test;
+
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import common.IInvokedMethodImpl;
+import common.WebBase;
+import pages.HomePage;
+import pages.LoginPage;
+
+@Listeners(IInvokedMethodImpl.class)
+public class LoginTest extends WebBase{
+	
+	@Test(dataProvider = "Login Data")
+	public void verifyLoginSuccessful(String username,String password)
+	{
+		LoginPage lp=new LoginPage();
+		lp.login(username, password);
+	}
+
+	
+}
